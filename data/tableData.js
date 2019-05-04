637e7e63700d731da31e356a7933fea046dd8e78
Katie Lewis <klewi314@gmail.com>
2019-05-04 13:58:20 -0700

    Added folders

---
+++ data/tableData.js
@@ -0,0 +1,16 @@
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+
+const tableArray = [
+  {
+    customerName: "Katie",
+    customerEmail: "katie@gmail.com",
+    customerID: "kayjlou22",
+    phoneNumber: "4154444444"
+  }
+];
+
+//Export the table array
+module.exports = tableArray;
